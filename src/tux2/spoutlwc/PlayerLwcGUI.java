2075f85fe6d0a05ef60f3929d4cf1b0fc57f8879
Joshua Reetz <josh100288@jrtechsupport.com>
2011-10-28 02:19:25 -0600

    More GUI Stuff...

--- src/tux2/spoutlwc/PlayerLwcGUI.java
+++ src/tux2/spoutlwc/PlayerLwcGUI.java
@@ -1,5 +1,7 @@
 package tux2.spoutlwc;
 
+import java.util.List;
+
 import org.bukkit.block.Block;
 import org.getspout.spoutapi.gui.Color;
 import org.getspout.spoutapi.gui.GenericButton;
@@ -9,30 +11,109 @@ import org.getspout.spoutapi.gui.GenericTextField;
 import org.getspout.spoutapi.gui.WidgetAnchor;
 import org.getspout.spoutapi.player.SpoutPlayer;
 
+import com.griefcraft.model.AccessRight;
 import com.griefcraft.model.Protection;
 
 public class PlayerLwcGUI {
 	
+	//All the Text boxes.
 	GenericTextField owner = new GenericTextField();
+	GenericTextField admins = new GenericTextField();
+	GenericTextField users = new GenericTextField();
+	GenericTextField password = new GenericTextField();
 	
 	public PlayerLwcGUI(SpoutLWC plugin, Protection protection, SpoutPlayer splayer) {
 		this(plugin, protection, splayer, protection.getBlock());
 	}
 	
 	public PlayerLwcGUI(SpoutLWC plugin, Protection protection, SpoutPlayer splayer, Block target) {
+		//Let's create a new popup
 		GenericPopup ppane = new GenericPopup();
+		//Add the label at the top of the window
 		GenericLabel label = new GenericLabel("LWC Locking System");
 		label.setTextColor(new Color(0, 200, 0)); //This makes the label green.
 		label.setAlign(WidgetAnchor.TOP_CENTER).setAnchor(WidgetAnchor.TOP_CENTER); //This puts the label at top center and align the text correctly.
 		label.shiftYPos(5);
-		GenericLabel olabel = new GenericLabel("Owner:");
-		olabel.setX(100).setY(10);
-		owner.setX(100).setY(60);
 		ppane.attachWidget(plugin, label);
+		//Create the owner label
+		GenericLabel olabel = new GenericLabel("Owner:");
+		//Set it's position on the screen (in pixels)
+		olabel.setX(10).setY(100);
+		//set it's position
+		olabel.setHeight(20);
+		//add the label to the popup
+		ppane.attachWidget(plugin, olabel);
+		//set properties on the Owner text box.
+		owner.setX(55).setY(95);
+		owner.setWidth(80).setHeight(15);
+		//Add the Owner Text box to the popup.
+		ppane.attachWidget(plugin, owner);
+		GenericLabel plabel = new GenericLabel("Password:");
+		plabel.setX(180).setY(100);
+		plabel.setHeight(20);
+		ppane.attachWidget(plugin, plabel);
+		password.setX(235).setY(95);
+		password.setWidth(80).setHeight(15);
+		ppane.attachWidget(plugin, password);
+		GenericLabel alabel = new GenericLabel("Admins:");
+		alabel.setX(10).setY(130);
+		alabel.setHeight(20);
+		ppane.attachWidget(plugin, alabel);
+		admins.setX(55).setY(125);
+		admins.setWidth(300).setHeight(15);
+		admins.setMaximumCharacters(500);
+		ppane.attachWidget(plugin, admins);
+		GenericLabel ulabel = new GenericLabel("Users:");
+		ulabel.setX(10).setY(160);
+		ulabel.setHeight(20);
+		ppane.attachWidget(plugin, ulabel);
+		users.setX(55).setY(155);
+		users.setWidth(300).setHeight(15);
+		users.setMaximumCharacters(500);
+		ppane.attachWidget(plugin, users);
 		GenericButton closebutton = new GenericButton("Close");
-		closebutton.setX(200).setY(210);
+		closebutton.setX(160).setY(210);
 		closebutton.setWidth(80).setHeight(20);
 		ppane.attachWidget(plugin, closebutton);
+		if(protection != null) {
+			owner.setText(protection.getOwner());
+			List<AccessRight> rights = protection.getAccessRights();
+			String sadmins = "";
+			String susers = "";
+			for(AccessRight right : rights) {
+				if(right.getType() == AccessRight.PLAYER) {
+					if(right.getRights() == AccessRight.RIGHT_ADMIN) {
+						//right.
+						if(!sadmins.equals("")) {
+							sadmins = sadmins + ", ";
+						}
+						sadmins = sadmins + right.getName();
+					}else if(right.getRights() == AccessRight.RIGHT_PLAYER) {
+						if(!susers.equals("")) {
+							susers = susers + ", ";
+						}
+						susers = susers + right.getName();
+					}
+				}else if(right.getType() == AccessRight.GROUP) {
+						if(right.getRights() == AccessRight.RIGHT_ADMIN) {
+							//right.
+							if(!sadmins.equals("")) {
+								sadmins = sadmins + ", ";
+							}
+							sadmins = sadmins + "g:" + right.getName();
+						}else if(right.getRights() == AccessRight.RIGHT_PLAYER) {
+							if(!susers.equals("")) {
+								susers = susers + ", ";
+							}
+							susers = susers + "g:" + right.getName();
+						}
+					}
+			}
+			admins.setText(sadmins);
+			//admins.setDirty(true);
+			users.setText(susers);
+			//users.setDirty(true);
+		}
 		splayer.getMainScreen().attachPopupScreen(ppane);
 	}
 
